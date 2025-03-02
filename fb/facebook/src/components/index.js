3df1b3f6026def24ea41f7eee1f23b78cb33b479
Joy macwan <joymacwan@zohomail.in>
2025-03-02 13:48:05 +0530

    Delete fb/facebook directory

--- fb/facebook/src/components/index.js
+++
@@ -1,8 +0,0 @@
-// src/index.js
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // Global styles if needed
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
